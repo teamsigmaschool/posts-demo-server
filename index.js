001918302b306d07bc3e2b7330a222fcab1cee6a
Sigma School <152258968+teamsigmaschool@users.noreply.github.com>
2026-05-18 08:29:31 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let path = require('path');
+
+let app = express()
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({Intro: "Hello World"});
+});
+
+app.listen(3000, () => {
+  console.log('App is listening on port 3000');
+});
