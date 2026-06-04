7d3940e5bdf25420b7563f6d34ac486d75254b9d
Reagan Kan <mjkan96@gmail.com>
2026-06-04 16:19:17 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-let express = require('express');
-let path = require('path');
+let express = require("express");
+let path = require("path");
 
-let app = express()
+let app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({Intro: "Hello World"});
+app.get("/", (req, res) => {
+  res.json({ Intro: "Hello World" });
 });
 
 app.listen(3000, () => {
-  console.log('App is listening on port 3000');
+  console.log("App is listening on port 3000");
 });
